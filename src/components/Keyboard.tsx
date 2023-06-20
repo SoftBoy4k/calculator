9d848e0574d4bd7af88a526bc6ec149dd6c92d32
Владислав Пимошенко <softboy@MacBook-Air-Vladislav.local>
2023-06-20 13:50:27 +0300

    made animation for pressing buttons

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -9,24 +9,24 @@ const Keyboard = ({isDarkTheme}: KeyboardProps) => {
     <ul className={`keyboard ${isDarkTheme ? "keyboard--dark" : "keyboard--light" }`}>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__main-item--dark" : "keyboard__main-item--light" }`}>AC</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__main-item--dark" : "keyboard__main-item--light" }`}>{isDarkTheme ? deleteArrowIconDark : deleteArrowIconLight}</li>
-      <li className="keyboard__item keyboard__math__signs">%</li>
-      <li className="keyboard__item keyboard__math__signs">÷</li>
+      <li className={`keyboard__item keyboard__math__signs ${isDarkTheme ? "keyboard__math__signs--dark" : "keyboard__math__signs--light"}`}>%</li>
+      <li className={`keyboard__item keyboard__math__signs ${isDarkTheme ? "keyboard__math__signs--dark" : "keyboard__math__signs--light"}`}>÷</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>7</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>8</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>9</li>
-      <li className="keyboard__item keyboard__math__signs">×</li>
+      <li className={`keyboard__item keyboard__math__signs ${isDarkTheme ? "keyboard__math__signs--dark" : "keyboard__math__signs--light"}`}>×</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>4</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>5</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>6</li>
-      <li className="keyboard__item keyboard__math__signs">-</li>
+      <li className={`keyboard__item keyboard__math__signs ${isDarkTheme ? "keyboard__math__signs--dark" : "keyboard__math__signs--light"}`}>-</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>1</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>2</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>3</li>
-      <li className="keyboard__item keyboard__math__signs">+</li>
+      <li className={`keyboard__item keyboard__math__signs ${isDarkTheme ? "keyboard__math__signs--dark" : "keyboard__math__signs--light"}`}>+</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>±</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>0</li>
       <li className={`keyboard__item ${isDarkTheme ? "keyboard__item--dark" : "keyboard__item--light"}`}>.</li>
-      <li className="keyboard__item keyboard__math__signs">=</li>
+      <li className={`keyboard__item keyboard__math__signs ${isDarkTheme ? "keyboard__math__signs--dark" : "keyboard__math__signs--light"}`}>=</li>
     </ul>
   )
 }
